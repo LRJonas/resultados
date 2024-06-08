6580861fc7c16b2637713b6d61943b715f062c04
Luqui05 <lucashtribeiro@hotmail.com>
2024-06-08 14:33:16 -0300

    feat: criado config para o consumer e listener

---
+++ src/main/java/com/guardioes/resultados/config/StringConsumerConfig.java
@@ -0,0 +1,37 @@
+package com.guardioes.resultados.config;
+
+import com.guardioes.resultados.entity.Resultado;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+public class StringConsumerConfig {
+    private final KafkaProperties properties;
+
+    @Bean
+    public ConsumerFactory<String, Resultado> consumerFactory() {
+        var configs = new HashMap<String, Object>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Resultado> strContainerFactory(
+            ConsumerFactory<String, Resultado> consumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, Resultado>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+}
--- src/main/java/com/guardioes/resultados/service/ResultadoService.java
+++ src/main/java/com/guardioes/resultados/service/ResultadoService.java
@@ -2,11 +2,19 @@ package com.guardioes.resultados.service;
 
 import com.guardioes.resultados.repository.ResultadoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class ResultadoService {
 
     private final ResultadoRepository repository;
+
+    @KafkaListener(groupId = "group-1", topics = "Resultado", containerFactory = "strContainerFactory")
+    public void listener(String message) {
+        log.info("Receive message {}", message);
+    }
 }
