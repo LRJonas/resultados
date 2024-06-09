22ae47fb79a2173a20f42a50f5e839ee0bf94de2
LRJonas <jonas.robertinho@gmail.com>
2024-06-09 19:10:24 -0300

    fix: alterações das config

--- src/main/java/com/guardioes/resultados/config/JsonConsumerConfig.java
+++ src/main/java/com/guardioes/resultados/config/JsonConsumerConfig.java
@@ -3,7 +3,7 @@ package com.guardioes.resultados.config;
 import com.guardioes.resultados.entity.Resultado;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +11,11 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,20 +24,22 @@ public class JsonConsumerConfig {
     private final KafkaProperties properties;
 
     @Bean
-    public ConsumerFactory<String, Object> jsonConsumerFactory() {
-        var configs = new HashMap<String, Object>();
+    public ConsumerFactory<String, Resultado> jsonConsumerFactory() {
+        Map<String, Object> configs = new HashMap<>();
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
-        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        configs.put("value.deserializer.type", Resultado.class);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        configs.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+        configs.put(JsonDeserializer.VALUE_DEFAULT_TYPE, Resultado.class.getName());
+        configs.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
         return new DefaultKafkaConsumerFactory<>(configs);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory jsonContainerFactory(
-            ConsumerFactory<String, Object> jsonConsumerFactory
-    ) {
-        var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
+    public ConcurrentKafkaListenerContainerFactory<String, Resultado> jsonContainerFactory(
+            ConsumerFactory<String, Resultado> jsonConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, Resultado> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(jsonConsumerFactory);
         factory.setRecordMessageConverter(new JsonMessageConverter());
         return factory;
--- src/main/java/com/guardioes/resultados/config/PropostaListener.java
+++ src/main/java/com/guardioes/resultados/config/PropostaListener.java
@@ -4,8 +4,6 @@ import com.guardioes.resultados.entity.Resultado;
 import com.guardioes.resultados.repository.ResultadoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -16,14 +14,9 @@ public class PropostaListener {
 
     private final ResultadoRepository repository;
 
-//    @KafkaListener(topics = "Resultado", groupId = "resultados")
-//    public void listen(Resultado resultado) {
-//        System.out.println("Nova proposta: " + resultado);
-//    }
-
     @KafkaListener(topics = "Resultado", groupId = "resultados", containerFactory = "jsonContainerFactory")
-    public void listen1(Resultado resultado) {
-        log.info("Proposta {}", resultado, " recebida com sucesso");
+    public void listen(Resultado resultado) {
+        log.info("Proposta {} recebida com sucesso", resultado);
 
         try {
             repository.save(resultado);
@@ -32,5 +25,4 @@ public class PropostaListener {
             log.error("Erro ao salvar proposta {} no banco de dados:", resultado.getId(), e);
         }
     }
-
 }
