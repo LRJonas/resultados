e24d8633e97eee069d007025d5fb726ffeedf070
Luqui05 <lucashtribeiro@hotmail.com>
2024-06-09 15:42:02 -0300

    feat: atualizando consumer kafka para receber objetos JSON

--- src/main/java/com/guardioes/resultados/config/PropostaListener.java
+++ src/main/java/com/guardioes/resultados/config/PropostaListener.java
@@ -1,14 +1,22 @@
 package com.guardioes.resultados.config;
 
 import com.guardioes.resultados.entity.Resultado;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PropostaListener {
+    private static final Logger log = LogManager.getLogger(PropostaListener.class);
 
-    @KafkaListener(topics = "Resultado", groupId = "resultados")
-    public void listen(Resultado resultado) {
-        System.out.println("Nova proposta: " + resultado);
+//    @KafkaListener(topics = "Resultado", groupId = "resultados")
+//    public void listen(Resultado resultado) {
+//        System.out.println("Nova proposta: " + resultado);
+//    }
+
+    @KafkaListener(topics = "Resultado", groupId = "resultados", containerFactory = "jsonContainerFactory")
+    public void listen1(Resultado resultado) {
+        log.info("Proposta {}", resultado, " recebida com sucesso");
     }
 }
