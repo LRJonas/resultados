df2d2cc45f739941e5026ce29f3d30c88bbcd3a7
LRJonas <jonas.robertinho@gmail.com>
2024-06-09 19:28:41 -0300

    fix: arrumando o fix anterior

--- src/main/java/com/guardioes/resultados/entity/Resultado.java
+++ src/main/java/com/guardioes/resultados/entity/Resultado.java
@@ -1,5 +1,6 @@
 package com.guardioes.resultados.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.persistence.*;
 import lombok.*;
@@ -17,10 +18,27 @@ public class Resultado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty("Titulo da Proposta")
     private String titulo;
+
+    @JsonProperty("Descricao da Proposta")
     private String descricao;
+
+    @JsonProperty("Sugestão de")
+    private String nome;
+
+    @JsonProperty("CPF do Funcionario")
+    private String funcionarioCpf;
+
+    @JsonProperty("Votos a favor")
     private int aprovar;
+
+    @JsonProperty("Votos contra")
     private int rejeitar;
+
+    @JsonProperty("Tempo para votação (em minutos)")
     private int tempo;
+
+    @JsonProperty("Proposta Ativa para votação")
     private boolean ativo;
 }
