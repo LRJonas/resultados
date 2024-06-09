6d3a38e83f25359670228feab1582a2fab14ad40
Luqui05 <lucashtribeiro@hotmail.com>
2024-06-09 17:22:24 -0300

    feat: repository save para as mensagens consumidas

--- src/main/java/com/guardioes/resultados/config/PropostaListener.java
+++ src/main/java/com/guardioes/resultados/config/PropostaListener.java
@@ -1,14 +1,20 @@
 package com.guardioes.resultados.config;
 
 import com.guardioes.resultados.entity.Resultado;
+import com.guardioes.resultados.repository.ResultadoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@Log4j2
 public class PropostaListener {
-    private static final Logger log = LogManager.getLogger(PropostaListener.class);
+
+    private final ResultadoRepository repository;
 
 //    @KafkaListener(topics = "Resultado", groupId = "resultados")
 //    public void listen(Resultado resultado) {
@@ -18,5 +24,6 @@ public class PropostaListener {
     @KafkaListener(topics = "Resultado", groupId = "resultados", containerFactory = "jsonContainerFactory")
     public void listen1(Resultado resultado) {
         log.info("Proposta {}", resultado, " recebida com sucesso");
+        repository.save(resultado);
     }
 }
