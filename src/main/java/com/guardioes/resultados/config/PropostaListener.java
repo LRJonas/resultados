3f4cc0c4df6767c18f6f1e246e378b029c3b3866
LRJonas <jonas.robertinho@gmail.com>
2024-06-09 14:47:51 -0300

    feat: implementação dos métodos list e save

--- src/main/java/com/guardioes/resultados/config/PropostaListener.java
+++ src/main/java/com/guardioes/resultados/config/PropostaListener.java
@@ -1,14 +1,22 @@
 package com.guardioes.resultados.config;
 
 import com.guardioes.resultados.entity.Resultado;
+import com.guardioes.resultados.repository.ResultadoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PropostaListener {
+
+    private final ResultadoRepository repository;
 
     @KafkaListener(topics = "Resultado", groupId = "resultados")
     public void listen(Resultado resultado) {
         System.out.println("Nova proposta: " + resultado);
+
+        repository.save(resultado);
+
     }
 }
--- src/main/java/com/guardioes/resultados/service/ResultadoService.java
+++ src/main/java/com/guardioes/resultados/service/ResultadoService.java
@@ -1,10 +1,14 @@
 package com.guardioes.resultados.service;
 
+import com.guardioes.resultados.entity.Resultado;
 import com.guardioes.resultados.repository.ResultadoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Log4j2
@@ -13,8 +17,13 @@ public class ResultadoService {
 
     private final ResultadoRepository repository;
 
-    @KafkaListener(groupId = "group-1", topics = "Resultado", containerFactory = "strContainerFactory")
+    /*@KafkaListener(groupId = "group-1", topics = "Resultado", containerFactory = "strContainerFactory")
     public void listener(String message) {
         log.info("Receive message {}", message);
+    }*/
+
+    @Transactional
+    public List<Resultado> findAll() {
+        return repository.findAll();
     }
 }
