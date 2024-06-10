b41cea05f6ab294fac1ef58f6c78761d6a1ba1bc
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-06-10 17:14:13 -0300

    test: Adição de teste para proposta inexistente

--- src/test/java/com/guardioes/resultados/repositorio/RepositorioResultadoTest.java
+++
@@ -1,7 +0,0 @@
-package com.guardioes.resultados.repositorio;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RepositorioResultadoTest {
-
-}
--- src/test/java/com/guardioes/resultados/web/controlador/TesteIntegracaoController.java
+++ src/test/java/com/guardioes/resultados/web/controlador/TesteIntegracaoController.java
@@ -1,6 +1,7 @@
 package com.guardioes.resultados.web.controlador;
 
 import com.guardioes.resultados.entidade.ResponseDtoPropostas;
+import com.guardioes.resultados.excecao.ExcecaoPropostaNaoEncontrada;
 import com.guardioes.resultados.servico.ServicoResultado;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.Collections;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -45,5 +47,18 @@ public class TesteIntegracaoController {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0]['Titulo da Proposta']").value("Teste"));
+    }
+
+    @Test
+    public void testBuscarPropostaInexistente() throws Exception {
+        when(servicoResultado.buscar("Teste")).thenThrow(new ExcecaoPropostaNaoEncontrada("Proposta não encontrada"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/resultados/{propostaTitulo}", "Teste")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.path").value("/api/v1/resultados/Teste"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.method").value("GET"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(404))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.statusText").value("Not Found"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Proposta não encontrada"));
     }
 }
